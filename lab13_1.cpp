5dc7d398e5723deaeb55741507bc25c0d69f787c
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-01-02 14:32:59 +0700

    Lab 13_1 Updated

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,24 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    B[4]=0,B[1]=0;
+    double sum=0,tick=1,har=0;
+    for(int i=0;i<=N-1;i++){
+        tick*=A[i];
+        sum+=A[i];
+        har+=(1/A[i]);
+        if(A[i] > B[4]) B[4] = A[i];
+        if(A[i] < B[5]) B[5] = A[i];
+    }
+    B[3] = N/har;
+    B[0] = sum/N;
+    B[5] = B[4];
+    for(int i=0;i<=N-1;i++){
+        if(A[i] < B[5]) B[5] = A[i];
+        B[1]+=pow((A[i]-B[0]),2);
+    }
+    B[1] = sqrt(B[1]/N);
+    B[2] = pow(tick,(1.0/N));
+}
